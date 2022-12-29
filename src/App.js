14576dd9f7310e18bece4dfc761f795f7f869756
yuliussetyawan <yuliusetyawan@gmail.com>
2022-12-29 23:10:18 +0700

    Creating Main Container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
-    <div className="App">
-    Hello
-    </div>
+    <Calculator/>
   );
 }
 
